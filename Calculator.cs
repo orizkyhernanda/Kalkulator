28c45e75c88492adb0ee9c56e160a62a3f15c781
Orizkyhernanda <rizkyhernanda14@gmail.com>
2019-03-19 11:56:00 +0700

    keterangan commit

---
+++ Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Method
+{
+    class Calculator
+    { 
+   
+        
+            public int penjumlahan(int a,int b)
+            {
+                return a + b;
+            }
+
+            public int penjumlahan(int a, int b, int c)
+            {
+                return a + b + c;     
+            }
+
+            public int pengurangan(int a, int b)
+            {
+                return a - b;
+            }
+
+            public int perkalian (int a, int b)
+            {
+                return a * b;
+            }
+
+
+        }
+}
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hasil;
+
+            Calculator calculator = new Calculator();
+
+            hasil = calculator.penjumlahan(10, 2);
+            CetakHasil("Hasil Penjumlahan : " + hasil);
+
+            hasil = calculator.penjumlahan(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan overload: " + hasil);
+
+            hasil = calculator.pengurangan(7, 2);
+            CetakHasil("Hasil pengurangan: " + hasil);
+
+            hasil = calculator.perkalian(5, 2);
+            CetakHasil("Hasil perkalian " + hasil);
+
+
+
+            Console.ReadKey();
+        }
+            static void CetakHasil(string hasil)
+            {
+                Console.WriteLine(hasil);
+            }
+            
+
+        }
+    }
+
